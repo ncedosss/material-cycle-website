9cc2e0ada661023347fc6cf4fcd5e5eaa0b0a9f8
nmatseke <nmatseke@finchoice.co.za>
2026-06-03 20:15:33 +0200

    Fix database

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,13 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
 
 pool.connect()
   .then(() => console.log("✅ PostgreSQL Connected"))
