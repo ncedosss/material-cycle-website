ced6f817df159cbb54de588ce2946b99d2f4bd48
nmatseke <nmatseke@finchoice.co.za>
2026-06-06 00:06:43 +0200

    Use process.env instead of vite front end

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,9 +6,9 @@ const jwt = require("jsonwebtoken");
 const transporter = require("../utils/email");
 
 const router = express.Router();
+const API_URL = process.env.VITE_API_URL || "http://localhost:5000";
 
 router.post("/register", async (req, res) => {
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
   try {
     const {
       firstName,
@@ -186,7 +186,6 @@ router.get("/verify-email/:token",async (req, res) => {
   }
 );
 router.post("/forgot-password",async (req, res) => {
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
     try {
       const { email } = req.body;
 
